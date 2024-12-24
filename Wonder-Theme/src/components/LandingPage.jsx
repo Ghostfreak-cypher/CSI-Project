3b56586469b7f19e035e0846244bd8da878733d4
Ghostfreak-cypher <vanshgoel979@gmail.com>
2024-12-24 20:36:08 +0530

    LandingPage

--- Wonder-Theme/src/components/LandingPage.jsx
+++ Wonder-Theme/src/components/LandingPage.jsx
@@ -3,13 +3,14 @@ import React from "react";
 function LandingPage() {
   return (
     <div>
-      <div className="Starcontainer flex justify-center relative align-center  items-center">
+      <div className="Starcontainer  flex justify-center relative align-center  items-center">
         <div className="Star mt-20  mr-10 rounded-full flex justify-center align-center  items-center w-[350px] h-[50px] bg-zinc-100">
-          <img className="star h-4 w-24 absolute left-[39%]"
+          <img
+            className="star h-4 w-24 absolute left-[39%]"
             src="https://cdn.prod.website-files.com/608acd17c4cd497d774d2c76/675b48cab6a43826473fe5f0_star-yellow-p-500.png"
             alt=""
           />
-          <h1 className="Text text-[17.5px] pl-24 font-thin text-black text-white">
+          <h1 className="Text text-[17.5px] pl-24 font-thin  text-black">
             +500 Happy Customers
           </h1>
         </div>
